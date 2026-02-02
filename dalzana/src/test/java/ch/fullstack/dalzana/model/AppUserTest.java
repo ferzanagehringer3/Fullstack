7e6c127a107b5078709d1d599bab6987aeea6bb0
ismdal <ismdal01@students.zhaw.ch>
2026-02-02 20:25:19 +0100

    db works on both devices

---
+++ dalzana/src/test/java/ch/fullstack/dalzana/model/AppUserTest.java
@@ -0,0 +1,22 @@
+// package ch.fullstack.dalzana.model;
+
+// import org.junit.jupiter.api.BeforeEach;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+// import org.springframework.test.web.servlet.MockMvc;
+// import ch.fullstack.dalzana.repository.AppUserRepository;
+
+// @SpringBootTest
+// @AutoConfigureMockMvc
+// public class AppUserTest {
+//             @Autowired
+//         private MockMvc mvc;
+
+//         @Autowired
+//         private AppUserRepository appUserRepository;
+
+//             @BeforeEach
+//         public void setup() {}
+//     
+// }
