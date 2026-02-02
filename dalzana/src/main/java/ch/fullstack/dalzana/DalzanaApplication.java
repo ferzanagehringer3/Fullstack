5392e77b3ee71cd81b68a770ea82fd099739a9af
ferzana.gehringer3 <gehrifer@students.zhaw.ch>
2026-02-02 13:28:36 +0100

    Springboot Proejkt erstellt

---
+++ dalzana/src/main/java/ch/fullstack/dalzana/DalzanaApplication.java
@@ -0,0 +1,13 @@
+package ch.fullstack.dalzana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalzanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalzanaApplication.class, args);
+	}
+
+}
