8200a60125565b4d741999f73173f48998c7e98a
ismdal <ismdal01@students.zhaw.ch>
2026-02-02 18:51:58 +0100

    frontend structure and other

--- dalzana/src/main/java/ch/fullstack/dalzana/controller/HelloController.java
+++ dalzana/src/main/java/ch/fullstack/dalzana/controller/HelloController.java
@@ -3,6 +3,7 @@ package ch.fullstack.dalzana.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,20 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("message", "Hello World – DalZana Connect 🚀");
         return "hello";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
     }
 }
