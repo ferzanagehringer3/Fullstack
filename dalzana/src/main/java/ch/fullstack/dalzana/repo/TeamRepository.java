e7599a4486f960f29aa7c0382730020f2715b4f8
ferzana.gehringer3 <gehrifer@students.zhaw.ch>
2026-02-03 10:06:34 +0100

    teams getestet

--- dalzana/src/main/java/ch/fullstack/dalzana/repo/TeamRepository.java
+++ dalzana/src/main/java/ch/fullstack/dalzana/repo/TeamRepository.java
@@ -3,4 +3,8 @@ package ch.fullstack.dalzana.repo;
 import ch.fullstack.dalzana.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRepository extends JpaRepository<Team, Long> { }
+import java.util.List;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findByRequestId(Long requestId);
+}
